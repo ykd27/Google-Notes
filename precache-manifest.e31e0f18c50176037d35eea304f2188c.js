07cb08600dfd58b3278506d680728a435dc05d4a
Yogendra Kumar Dubey <ykdykd27@gmail.com>
2024-03-25 10:55:51 +0530

    Updates

---
+++ precache-manifest.e31e0f18c50176037d35eea304f2188c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fb614105698c0d9ccd3",
+    "url": "/demo/static/css/main.2567844f.chunk.css"
+  },
+  {
+    "revision": "3fb614105698c0d9ccd3",
+    "url": "/demo/static/js/main.3fb61410.chunk.js"
+  },
+  {
+    "revision": "45c39b89840e28a76616",
+    "url": "/demo/static/js/1.45c39b89.chunk.js"
+  },
+  {
+    "revision": "df776aaf92209c815866",
+    "url": "/demo/static/js/runtime~main.df776aaf.js"
+  },
+  {
+    "revision": "f5b7d001a4653f05b111cfbba2a673b6",
+    "url": "/demo/index.html"
+  }
+];
